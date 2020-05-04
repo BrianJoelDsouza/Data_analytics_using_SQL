63ab86796aabb484c17126934da638d88f93af84
Brian Dsouza <53157916+BrianJoelDsouza@users.noreply.github.com>
2020-05-04 15:41:35 -0700

    Edited the abstract

--- bikeshare.sql
+++ bikeshare.sql
@@ -1,4 +1,5 @@
-/* Abstract: */
+/* Abstract: This project analyzed the San Francisco bike share data from Google Big Query and provided recommendations to 
+improve the service, by answering crucial business questions. */
 --STATIONS:
 --Which stations have the highest number of docks?
 SELECT
